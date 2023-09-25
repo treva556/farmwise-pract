f59b8de3b82e0a70873f3949a9bbda23b89a0d42
treva556 <marktreva92@gmail.com>
2023-09-26 02:33:25 +0300

    ll

--- src/components/body.js
+++ src/components/body.js
@@ -48,6 +48,76 @@ function Bodyy() {
               <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
             </div>
           </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
+          <Link to="/farmproduce/cereals">
+            <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
+              <img
+                src={pic1}
+                alt="Cereals"
+                className="mx-auto h-64 w-64 object-contain" // Adjusted image size
+              />
+              <h1 className="text-center font-bold md:text-base mt-2">Cereals</h1>
+            </div>
+          </Link>
           <Link to="/farmproduce/fruits">
             <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
               <img
