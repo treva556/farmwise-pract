9abd142e054b8f5d6fbd4bb2aa8206686924b1ef
treva556 <marktreva92@gmail.com>
2023-09-28 18:53:03 +0300

    show

--- src/Seller/Sellerform.js
+++ src/Seller/Sellerform.js
@@ -1,10 +1,130 @@
-import React from "react";
- 
-export function Sellerform() {
+
+import React from 'react';
+
+const Sellerform = () => {
   return (
-    <>
-    </>
+    <div className="relative flex flex-col justify-center min-h-screen overflow-hidden ">
+      <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring-2 ring-indigo-600 lg:max-w-xl">
+        <h1 className="text-3xl font-semibold text-center text-indigo-700  uppercase decoration-wavy">
+          Merchant Form
+        </h1>
+        <form className="mt-6">
+          <div className="mb-2">
+            <label>
+              <span className="text-gray-700">Your name</span>
+              <input
+                type="text"
+                name="name"
+                className="
+
+            w-full
+            block px-16 py-2 mt-2
+            border-gray-300
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+          "
+                placeholder="John cooks"
+              />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              <span className="text-gray-700">Email address</span>
+              <input
+                name="email"
+                type="email"
+                className="
+            block
+            w-full
+            mt-2 px-16 py-2
+            border-gray-300
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+          "
+                placeholder="john.cooks@example.com"
+                required
+              />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              <span className="text-gray-700">Contact</span>
+              <input
+                name="email"
+                type="email"
+                className="
+            block
+            w-full
+            mt-2 px-16 py-2
+            border-gray-300
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+          "
+                placeholder="0795115545"
+                required
+              />
+            </label>
+          </div>
+          <div className="mb-2">
+            <label>
+              <span className="text-gray-700">County</span>
+              <input
+                name="email"
+                type="email"
+                className="
+            block
+            w-full
+            mt-2 px-16 py-2
+            border-gray-300
+            rounded-md
+            shadow-sm
+            focus:border-indigo-300
+            focus:ring
+            focus:ring-indigo-200
+            focus:ring-opacity-50
+          "
+                placeholder="john.cooks@example.com"
+                required
+              />
+            </label>
+          </div>
+
+          <div class="mb-6">
+            <button
+              type="submit"
+              className="
+            h-10
+            px-5
+            text-indigo-100
+            bg-indigo-700
+            rounded-lg
+            transition-colors
+            duration-150
+            focus:shadow-outline
+            hover:bg-indigo-800
+          "
+            >
+              Register Shop
+            </button>
+          </div>
+          <div></div>
+        </form>
+      </div>
+    </div>
   );
-}
+};
 
 export default Sellerform;
+ 
