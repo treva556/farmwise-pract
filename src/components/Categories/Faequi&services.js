28368a8dd9df05b108dbd439d55ef75752fe07ca
treva556 <marktreva92@gmail.com>
2023-09-28 05:37:30 +0300

    show

--- src/components/Categories/Faequi&services.js
+++ src/components/Categories/Faequi&services.js
@@ -8,34 +8,34 @@ function EquipandServices() {
   return (
     <div>
       <h1>Farm Equipment and Services</h1>
-      <Link to="/equip&sevices/services">
+      <Link to="/equip&services/services">
         <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
           <img
             src={pic1}
             alt="Services"
             className="mx-auto h-64 w-64 object-contain"
           />
-          <h1 className="text-center font-bold md:text-base mt-2">Meat</h1>
+          <h1 className="text-center font-bold md:text-base mt-2">Services</h1>
         </div>
       </Link>
-      <Link to="/animalproduce/fish">
+      <Link to="/equip&services/farmequipment">
         <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
           <img
             src={pic1}
             alt="Farm Equipment"
             className="mx-auto h-64 w-64 object-contain"
           />
-          <h1 className="text-center font-bold md:text-base mt-2">Fish</h1>
+          <h1 className="text-center font-bold md:text-base mt-2">Farm Equipment</h1>
         </div>
       </Link>
-      <Link to="/animalproduce/poultry">
+      <Link to="/equip&services/animalequipment">
         <div className="w-full shadow-sm p-5 rounded-md cursor-pointer hover:border-green-500">
           <img
             src={pic1}
             alt="Animal Equipment"
             className="mx-auto h-64 w-64 object-contain"
           />
-          <h1 className="text-center font-bold md:text-base mt-2">Poultry</h1>
+          <h1 className="text-center font-bold md:text-base mt-2">Animal Equipment</h1>
         </div>
       </Link>
        Add more links for other categories
