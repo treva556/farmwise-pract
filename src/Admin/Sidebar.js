c49fd04c2f72dc113d282674ad162bf42afd632c
treva556 <marktreva92@gmail.com>
2023-09-28 17:13:24 +0300

    show

--- src/Admin/Sidebar.js
+++ src/Admin/Sidebar.js
@@ -11,42 +11,25 @@ function Sidebar() {
 
        <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
 
-<div class="min-h-screen flex flex-row bg-gray-100">
-  <div class="flex flex-col w-56 bg-white rounded-r-3xl overflow-hidden">
+<div class="min-h-screen flex flex-row bg-green-100">
+  <div class="flex flex-col w-56 bg-green-600 rounded-r-3xl overflow-hidden">
     <div class="flex items-center justify-center h-20 shadow-md">
       <h1 class="text-3xl uppercase text-indigo-500">Logo</h1>
     </div>
     <ul class="flex flex-col py-4">
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-home"></i></span>
+        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white-500 hover:text-white-800">
+          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-white-400"><i class="bx bx-home"></i></span>
           <span class="text-sm font-medium">Dashboard</span>
         </a>
       </li>
       <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-music"></i></span>
-          <span class="text-sm font-medium">Music</span>
+        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white-500 hover:text-gray-800">
+          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-white-400"><i class="bx bx-music"></i></span>
+          <span class="text-sm font-medium">Pending</span>
         </a>
       </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-drink"></i></span>
-          <span class="text-sm font-medium">Drink</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-shopping-bag"></i></span>
-          <span class="text-sm font-medium">Shopping</span>
-        </a>
-      </li>
-      <li>
-        <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
-          <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-chat"></i></span>
-          <span class="text-sm font-medium">Chat</span>
-        </a>
-      </li>
+    
       <li>
         <a href="#" class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800">
           <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400"><i class="bx bx-user"></i></span>
