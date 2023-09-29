6c5559f0b667f4f8180a554d3cf57db793a3aa36
treva556 <marktreva92@gmail.com>
2023-09-29 13:16:44 +0300

    show

--- src/Seller/Shop.js
+++ src/Seller/Shop.js
@@ -6,15 +6,20 @@ function Sellershop() {
           <div className=" w-screen h-[3cm] bg-green-900 text bg-center flex">
       {/* Your content here */}
    
-      <div>
+      <div className=" mr-10">
       <p className=" text-white">Mark Trevor </p>
       <p className=" text-white">0790890098 </p>
       <p className=" text-white">marktreva23@gmail.com</p>
       </div>
       <div className=" ml-11">
-        <button>
-         Add Item
-        </button>
+      <button class="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">
+  Add Item
+</button>
+      </div>
+      <div className=" ml-11">
+      <button class="bg-yellow-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">
+  Delete Shop
+</button>
       </div>
 
     </div>
