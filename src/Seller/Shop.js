f424d6c7a5cb22f42d0e785dd81024985fa4b80b
treva556 <marktreva92@gmail.com>
2023-09-29 13:12:46 +0300

    show

--- src/Seller/Shop.js
+++ src/Seller/Shop.js
@@ -5,17 +5,16 @@ function Sellershop() {
       <div>
           <div className=" w-screen h-[3cm] bg-green-900 text bg-center flex">
       {/* Your content here */}
-      <div>
-  ggggggggggg
-      </div>
-      
+   
       <div>
       <p className=" text-white">Mark Trevor </p>
       <p className=" text-white">0790890098 </p>
       <p className=" text-white">marktreva23@gmail.com</p>
       </div>
-      <div>
-        hhhhhhhh
+      <div className=" ml-11">
+        <button>
+         Add Item
+        </button>
       </div>
 
     </div>
