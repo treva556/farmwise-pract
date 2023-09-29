3809307114089875e6a20ba81dc654f14e6df025
treva556 <marktreva92@gmail.com>
2023-09-29 13:07:42 +0300

    show

--- src/Seller/Shop.js
+++ src/Seller/Shop.js
@@ -3,9 +3,21 @@ import React from "react";
 function Sellershop() {
     return (
       <div>
-          <div className=" w-screen h-[3cm] bg-green-900 text bg-center">
+          <div className=" w-screen h-[3cm] bg-green-900 text bg-center flex">
       {/* Your content here */}
-      <p className=""> </p>
+      <div>
+  ggggggggggg
+      </div>
+      
+      <div>
+      <p className=" text-white">Mark Trevor </p>
+      <p className=" text-white">0790890098 </p>
+      <p className=" text-white">marktreva23@gmail.com</p>
+      </div>
+      <div>
+        hhhhhhhh
+      </div>
+
     </div>
         <p>
 
